915683c058cb96e82c90dfe1058a5e2a887b4d05
Cayden R. Haun <floridaman7588@gmail.com>
2023-07-26 21:25:56 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,7 @@
 
 
 # Build the project.
-hugo build \
-    --buildDrafts \
-    --baseURL ${HUGO_BASEURL} \
-    --minify \
-    --gc
+hugo --buildDrafts --baseURL ${HUGO_BASEURL} --minify --gc
 
 # Copy to nginx directory.
 cp -r public/* /usr/share/nginx/html/
